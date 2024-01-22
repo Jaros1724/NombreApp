b4df803143bb9f01638a133557899972a021c5ea
Jaros1724 <jarosrodriguez717@gmail.com>
2024-01-22 17:51:52 -0500

    trabajo 1

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -10,16 +10,17 @@ export class IntroPage {
 
   constructor(private router: Router) { }
   slides = [
-    {
-      title: "Welcome to the Docs!",
-      description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
-      image: "../../assets/images/1.webp",
-      help_text: "For more information on Ionic, please see <a href='https://ionicframework.com/docs/'>our docs</a>.",
-    },
+
     {
       title: "What is Ionic?",
       description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
       image: "../../assets/images/2.webp",
+      help_text: "For more information on Ionic, please see <a href='https://ionicframework.com/docs/'>our docs</a>.",
+    },
+    {
+      title: "What is Ionic Cloud?",
+      description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+      image: "../../assets/images/3.webp",
       help_text: "For more information on Ionic, please see <a href='https://ionicframework.com/docs/'>our docs</a>.",
     },
     {
